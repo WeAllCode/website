4218ac78aa9b18c629501a9689fc2fcef3089d4e
bkellgren <bkellgren@gmail.com>
2014-06-24 18:48:56 -0500

    course details
    Former-commit-id: d716b26a952006d333fc42681ce4fbded87665bd

--- coderdojochi/models.py
+++ coderdojochi/models.py
@@ -47,6 +47,7 @@ class Student(models.Model):
         return self.user.username
 
 class Class(models.Model):
+    course = models.CharField(max_length=255, blank=True, null=True)
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=40, blank=True, null=True)
     description = models.TextField(blank=True, null=True, help_text="Basic HTML allowed")
