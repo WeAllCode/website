c1523eb3dab772b0e08c13655de5ee287a1c0326
bkellgren <bkellgren@gmail.com>
2014-07-11 18:19:35 -0500

    lots of fun refactoring
    Former-commit-id: bb493436f3472ca381176efbd97fbcc04a4b1f9a

--- coderdojochi/admin.py
+++ coderdojochi/admin.py
@@ -1,10 +1,13 @@
 from django.contrib.auth import get_user_model
 from django.contrib import admin
-from coderdojochi.models import Mentor, Student, Class
+from coderdojochi.models import Mentor, Guardian, Student, Course, Session, Order
 
 User = get_user_model()
 
 admin.site.register(User)
 admin.site.register(Mentor)
+admin.site.register(Guardian)
 admin.site.register(Student)
-admin.site.register(Class)
+admin.site.register(Course)
+admin.site.register(Session)
+admin.site.register(Order)
