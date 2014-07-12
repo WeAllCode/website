c6c6ed90fa5ee9a8efdbc7c907d8538703b57749
bkellgren <bkellgren@gmail.com>
2014-07-12 15:25:31 -0500

    big ass check in

--- coderdojochi/forms.py
+++ coderdojochi/forms.py
@@ -2,11 +2,13 @@ from django import forms
 from django.forms import Form, ModelForm
 from coderdojochi.models import Mentor, Guardian, Student
 
+import html5.forms.widgets as html5_widgets
+
 class MentorForm(ModelForm):
 
-    first_name = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'First Name','class': 'form-control'}))
-    last_name = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Last Name','class': 'form-control'}))
-    bio = forms.CharField(label='', widget=forms.Textarea(attrs={'placeholder': 'Short Bio','class': 'form-control', 'rows': 5}))
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'First Name','class': 'form-control'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Last Name','class': 'form-control'}))
+    bio = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Short Bio','class': 'form-control', 'rows': 5}))
 
     class Meta:
         model = Mentor
@@ -15,9 +17,9 @@ class MentorForm(ModelForm):
 
 class GuardianForm(ModelForm):
 
-    first_name = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'First Name','class': 'form-control'}))
-    last_name = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Last Name','class': 'form-control'}))
-    phone = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Phone Number','class': 'form-control'}))
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'First Name','class': 'form-control'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Last Name','class': 'form-control'}))
+    phone = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Phone Number','class': 'form-control'}))
 
     class Meta:
         model = Guardian
@@ -25,10 +27,10 @@ class GuardianForm(ModelForm):
 
 class StudentForm(ModelForm):
 
-    first_name = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'First Name','class': 'form-control'}))
-    last_name = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Last Name','class': 'form-control'}))
-    gender = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Gender','class': 'form-control'}))
+    birthday = forms.CharField(widget=html5_widgets.DateInput(attrs={'placeholder': 'Gender','class': 'form-control'}))
+    photo_release = forms.BooleanField(widget=forms.CheckboxInput(attrs={'required': 'required'}), label="Photo Consent", help_text="I hereby give permission to CoderDojoChi to use the student's image and/or likeness in promotional materials.")
+    consent = forms.BooleanField(widget=forms.CheckboxInput(attrs={'required': 'required'}), label="General Consent", help_text="I hereby give consent for the student signed up above to participate in CoderDojoChi.")
 
     class Meta:
         model = Student
-        fields = ('first_name', 'last_name', 'gender')
+        exclude = ('guardian', 'created_at', 'updated_at', 'active')
--- coderdojochi/templatetags/coderdojochi_extras.py
+++ coderdojochi/templatetags/coderdojochi_extras.py
@@ -1,6 +1,28 @@
 from django import template
+from django.core.urlresolvers import reverse
+
+from coderdojochi.models import Order
+
 register = template.Library()
 
 @register.filter
 def subtract(value, arg):
     return value - arg
+
+
+@register.simple_tag
+def student_register_link(student, session):
+
+    orders = Order.objects.filter(student=student, session=session)
+
+    if orders.count():
+        button_type = 'danger'
+        button_msg = 'Can\'t make it'
+    else:
+        button_type = 'success'
+        button_msg = 'Enroll'
+
+    link = form = '<a href="' + reverse('session_sign_up', args=(session.start_date.year, session.start_date.month, session.start_date.day, session.course.slug, student.id)) + '" class="btn btn-' + button_type + '">' + button_msg + '</a>'
+
+    return form
+
