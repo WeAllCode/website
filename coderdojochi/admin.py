b0346dc9d8400fab479526e5d1587f28d6914037
kapil1garg <kapil1garg@gmail.com>
2014-09-11 13:51:12 -0500

    Added Equipment Model and Donation Page
    Former-commit-id: d068cc6a52d1f8313a28902dbbf45a8cb0b7c058

--- coderdojochi/admin.py
+++ coderdojochi/admin.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.contrib import admin
-from coderdojochi.models import Mentor, Guardian, Student, Course, Session, Order
+from coderdojochi.models import Mentor, Guardian, Student, Course, Session, Order, EquipmentType, EquipmentTable
 
 User = get_user_model()
 
@@ -11,3 +11,5 @@ admin.site.register(Student)
 admin.site.register(Course)
 admin.site.register(Session)
 admin.site.register(Order)
+admin.site.register(EquipmentType)
+admin.site.register(EquipmentTable)
