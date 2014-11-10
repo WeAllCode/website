56df781f3afaa13bc79c6b9f99614907c58586f0
Brett Kellgren <bkellgren@Bretts-MacBook-Pro.local>
2014-11-09 18:59:46 -0600

    small buttons and details
    Former-commit-id: 58b4afd8af16fb27949e77c04585f7c909d49c58

--- coderdojochi/templatetags/coderdojochi_extras.py
+++ coderdojochi/templatetags/coderdojochi_extras.py
@@ -16,13 +16,13 @@ def student_register_link(student, session):
     orders = Order.objects.filter(student=student, session=session)
 
     if orders.count():
-        button_modifier = ' danger'
+        button_modifier = ' btn-cdc-danger'
         button_msg = 'Can\'t make it'
     else:
         button_modifier = ''
         button_msg = 'Enroll'
 
-    link = form = '<a href="' + reverse('session_sign_up', args=(session.start_date.year, session.start_date.month, session.start_date.day, session.course.slug, student.id)) + '" class="btn-cdc btn-cdc-sm'+ button_modifier + '">' + button_msg + '</a>'
+    link = form = '<a href="' + reverse('session_sign_up', args=(session.start_date.year, session.start_date.month, session.start_date.day, session.course.slug, session.course.id, student.id)) + '" class="btn-cdc btn-cdc-sm'+ button_modifier + '">' + button_msg + '</a>'
 
     return form
 
