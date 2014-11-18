c6a8303269990667ec867d3d5a4536c1659dfae5
Brett Kellgren <bkellgren@Bretts-MacBook-Pro.local>
2014-11-17 23:43:23 -0600

    build updates, gruntfile changes, topnav updates, refactor sass setup, donate page updates
    Former-commit-id: 9aa6a1ec86bde4f802a1c2dbd0c8cba2808b60ee

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,11 +2,8 @@ module.exports = function(grunt) {
     'use strict';
 
     // load custom tasks
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-compass');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
+    require('time-grunt')(grunt);
+    require('load-grunt-tasks')(grunt);
 
     // initialize the configuration
     grunt.initConfig({
@@ -22,40 +19,24 @@ module.exports = function(grunt) {
             js: '<%= files.static %>/js',
             img: '<%= files.static %>/images',
             sprites: '<%= files.static %>/sprites',
-            builtJS: '<%= files.static %>/build/js',
-            builtCSS: '<%= files.static %>/build/css',
             sourceMap: 'cdc.min.map'
         },
 
-        watch: {
-            // scripts: {
-            //     files: [
-            //         '<%= files.grunt %>',
-            //         '<%= files.js %>/**/*.js'
-            //     ],
-            //     tasks: ['jshint'],
-            //     options: {
-            //         livereload: true
-            //     }
-            // },
-            scss: {
-                files: ['<%= files.sass %>/**/*.scss'],
-                tasks: ['compass:dev']
+        autoprefixer: {
+
+            options: {
+                map: true,
+                browsers: ['Firefox > 25', 'last 4 versions', '> 3%', 'ie 8']
             },
-            markup: {
-                files: [
-                    '<%= files.templates %>/**/*.html',
-                    '**/*.{html,php}'
+
+            dev: { 
+                expand: true,
+                flatten: true,
+                src: [
+                    '<%= files.css %>/*.css', 
+                    '!<%= files.css %>/*.min.css'
                 ],
-                options: {
-                    livereload: true
-                }
-            },
-            livereload: {
-                files: ['<%= files.css %>/**/*.css'],
-                options: {
-                    livereload: true
-                }
+                dest: '<%= files.css %>/' 
             }
         },
 
@@ -89,57 +70,152 @@ module.exports = function(grunt) {
             }
         },
 
+        concurrent: {
+            dev : [
+                'watch', 
+                'shell:django'
+            ],
+            options: {
+                logConcurrentOutput: true
+            }
+        },
+
         cssmin: {
             minify: {
                 src: [
                     '<%= files.css %>/cdc.css'
                 ],
-                dest: '<%= files.builtCSS %>/cdc.min.css'
+                dest: '<%= files.css %>/cdc.min.css'
             }
         },
 
+        imageoptim: {
+            src: [
+                '<%= files.img %>'
+            ],
+            options: {
+                jpegMini: true,
+                imageAlpha: true,
+                quitAfter: true
+            }
+        },
+
+        jsbeautifier: {
+            files: [
+                '<%= files.js %>/**/*.js',
+                '!<%= files.js %>/**/*.min.js',
+                '!<%= files.js %>/vendor/**/*.js'
+            ]
+        },
+
         jshint: {
-            files: ['<%= files.js %>/*.js'],
+            files: [
+                '<%= files.js %>/**/*.js'
+            ],
             options: {
                 forin: true,
                 noarg: true,
                 noempty: true,
                 eqeqeq: true,
                 bitwise: true,
-                strict: false,
-                undef: false,
+                strict: true,
+                undef: true,
                 curly: true,
                 expr: true,
                 browser: true,
                 jquery: true,
-                devel: true,
-                maxerr: 100,
+                es3: true,
                 ignores: [
-                    '<%= files.js %>/*.min.js'
+                    '<%= files.js %>/*.min.js',
+                    '<%= files.js %>/vendor/**/*.js'
                 ],
                 // ignore certain warnings
-                '-W040': true
+                '-W097': true, // strict must be contained in function (browserify will wrap them on compile)
+                '-W117': true, // globals are not defined
+                '-W069': true, // arrays should be in dot notation
+                '-W016': true, // Unexpected use of '>>>'.
+                '-W061': true  // eval can be harmful
+            }
+        },
+
+        shell: {
+            django: {
+                command: 'python manage.py runserver 0.0.0.0:8000',
+                options: {
+                    stdout: true,
+                    stdin: true,
+                    stderr: true
+                }
             }
         },
 
         uglify: {
-            deploy: {
+            dist: {
                 options: {
-                    sourceMap: '<%= files.builtJS %>/<%= files.sourceMap %>',
-                    sourceMappingURL: '<%= files.sourceMap %>',
-                    mangle: false
+                    sourceMap: true,
+                    sourceMapName: '<%= files.js %>/cdc.min.map'
                 },
                 files: {
-                    '<%= files.builtJS %>/cdc.min.js': [
+                    '<%= files.js %>/cdc.min.js': [
                         '<%= files.js %>/vendor/*.js',
-                        '<%= files.js %>/*.js'
+                        '<%= files.js %>/*.js',
+                        '!<%= files.js %>/*.min.js'
                     ]
                 }
             }
+        },
+
+        watch: {
+            
+            scripts: {
+                files: [
+                    '<%= files.grunt %>',
+                    '<%= files.js %>/**/*.js',
+                    '!<%= files.js %>/*.min.js',
+                    '!<%= files.js %>/vendor/*.js',
+                ],
+                tasks: [
+                    'jsbeautifier',
+                    'jshint'
+                ],
+                options: {
+                    livereload: true
+                }
+            },
+            
+            scss: {
+                files: ['<%= files.sass %>/**/*.scss'],
+                tasks: [
+                    'compass:dev',
+                    'autoprefixer:dev'
+                ]
+            },
+
+            css: {
+                files: ['<%= files.css %>/**/*.css'],
+                options: {
+                    livereload: true
+                }
+            }
         }
+
     });
 
-    // default task
-    grunt.registerTask('default', ['watch']);
-    grunt.registerTask('deploy', ['compass:dist', 'cssmin', 'jshint', 'uglify']);
+    
+    // register tasks
+    
+    grunt.registerTask('default', [ // $ grunt
+        'jsbeautifier',
+        'jshint',
+        'concurrent:dev'
+    ]);
+
+    grunt.registerTask('build', [ // $ grunt build
+        'jsbeautifier',
+        'jshint',
+        'uglify:dist',
+        'compass:dist',
+        'autoprefixer:dev',
+        'cssmin'
+    ]);
 };
