f69d18fb808ff722e365ab5d1b2243161b679f0d
Brett Kellgren <bkellgren@gmail.com>
2015-01-04 19:41:36 -0600

    adjust instances where upcomign sessions are shown to filter by date

--- coderdojochi/views.py
+++ coderdojochi/views.py
@@ -106,7 +106,7 @@ def add_months(sourcedate, months):
 
 def home(request, template_name="home.html"):
 
-    upcoming_classes = Session.objects.filter(active=True).order_by('start_date')[:3]
+    upcoming_classes = Session.objects.filter(active=True, end_date__gte=datetime.now()).order_by('start_date')[:3]
 
     return render_to_response(template_name, {
         'upcoming_classes': upcoming_classes
@@ -252,7 +252,7 @@ def sessions(request, year=False, month=False, template_name="sessions.html"):
     prev_date = add_months(calendar_date,-1)
     next_date = add_months(calendar_date,1)
 
-    all_sessions = Session.objects.filter(active=True).order_by('start_date')
+    all_sessions = Session.objects.filter(active=True, end_date__gte=datetime.now()).order_by('start_date')
     sessions = all_sessions.filter(start_date__year=year, start_date__month=month).order_by('start_date')
     cal = SessionsCalendar(sessions).formatmonth(year, month)
 
@@ -304,7 +304,7 @@ def session_detail(request, year, month, day, slug, session_id, template_name="s
 
         return HttpResponseRedirect(reverse('session_detail', args=(session_obj.start_date.year, session_obj.start_date.month, session_obj.start_date.day, session_obj.course.slug, session_obj.id)))
 
-    upcoming_classes = Session.objects.filter(active=True).order_by('start_date')
+    upcoming_classes = Session.objects.filter(active=True, end_date__gte=datetime.now()).order_by('start_date')
 
     if request.user.is_authenticated():
         if request.user.role == 'mentor':
@@ -517,10 +517,10 @@ def dojo(request, template_name="dojo.html"):
             account = mentor
 
             mentor_sessions = Session.objects.filter(mentors=mentor)
-            upcoming_sessions = mentor_sessions.filter(active=True).order_by('start_date')
-            past_sessions = mentor_sessions.exclude(active=True).order_by('start_date')
+            upcoming_sessions = mentor_sessions.filter(active=True, end_date__gte=datetime.now()).order_by('start_date')
+            past_sessions = mentor_sessions.filter(active=True, end_date__lte=datetime.now()).order_by('start_date')
 
-            upcoming_meetings = Meeting.objects.filter(active=True).order_by('start_date')
+            upcoming_meetings = Meeting.objects.filter(active=True, end_date__gte=datetime.now()).order_by('start_date')
 
 
             if request.method == 'POST':
@@ -544,8 +544,8 @@ def dojo(request, template_name="dojo.html"):
 
             students = Student.objects.filter(guardian=guardian)
             student_orders = Order.objects.filter(student__in=students)
-            upcoming_orders = student_orders.filter(active=True).order_by('session__start_date')
-            past_orders = student_orders.exclude(active=True).order_by('session__start_date')
+            upcoming_orders = student_orders.filter(active=True, end_date__gte=datetime.now()).order_by('session__start_date')
+            past_orders = student_orders.filter(active=True, end_date__gte=datetime.now()).order_by('session__start_date')
 
             if request.method == 'POST':
                 form = GuardianForm(request.POST, instance=account)
@@ -729,8 +729,8 @@ def cdc_admin(request, template_name="cdc-admin.html"):
 
     sessions = Session.objects.all()
 
-    upcoming_sessions = sessions.filter(active=True).order_by('start_date')
-    past_sessions = sessions.exclude(active=True).order_by('start_date')
+    upcoming_sessions = sessions.filter(active=True, end_date__gte=datetime.now()).order_by('start_date')
+    past_sessions = sessions.filter(active=True, end_date__lte=datetime.now()).order_by('start_date')
 
     return render_to_response(template_name,{
         'upcoming_sessions': upcoming_sessions,
@@ -802,7 +802,6 @@ def session_check_in(request, session_id, template_name="session-check-in.html")
     return render_to_response(template_name,{
         'session': session_obj,
     }, context_instance=RequestContext(request))
-
 
 
 def sendSystemEmail(request, subject, template_name, merge_vars):
