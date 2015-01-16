7b52f75429f08226a2fba51d11fbede7cfb89ed9
Brett Kellgren <bkellgren@gmail.com>
2015-01-15 23:08:54 -0600

    set up AppConfig ready on app init, mentors not "public" until avatar submitted and moderated by site admins (fixed #40)
    Former-commit-id: b13611ac253397166f4a5cf5fa313c22eec0ebef

---
+++ coderdojochi/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+class CoderDojoChiConfig(AppConfig):
+    name = 'coderdojochi'
+    verbose_name = "CoderDojoChi"
+
+    def ready(self):
+        import coderdojochi.signals_handlers
---
+++ coderdojochi/signals_handlers.py
@@ -0,0 +1,24 @@
+from django.conf import settings
+from django.core.mail import EmailMultiAlternatives
+from django.dispatch import receiver
+
+from avatar.signals import avatar_updated
+
+from coderdojochi.models import Mentor 
+
+@receiver(avatar_updated)
+def avatar_updated_handler(sender, user, avatar, **kwargs):
+    
+    mentor = Mentor.objects.get(user=user)
+    mentor.public = False
+    mentor.save()
+
+    msg = EmailMultiAlternatives(
+        subject='Mentor Avatar Changed',
+        body='Mentor with email ' + mentor.user.email + ' changed their avatar image.  Please approve (' + settings.SITE_URL + '/mentors/' + str(mentor.id) + '/activate/) or reject (' + settings.SITE_URL + '/mentors/' + str(mentor.id) + '/reject-image/).',
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=[v for k,v in settings.ADMINS]
+    )
+    msg.attach_alternative('<h1>Is this avatar okay?</h1><img src="' + avatar.avatar_url(400) + '"><h2><a href="' + settings.SITE_URL + '/mentors/' + str(mentor.id) + '/activate/">Allow</a></h2><h2><a href="' + settings.SITE_URL + '/mentors/' + str(mentor.id) + '/reject-image/">Deny</a></h2>', 'text/html')
+
+    msg.send()
