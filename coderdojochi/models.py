ce0ad4b557a276d0fac5545f65d3ac1c410c97ca
Brett Kellgren <bkellgren@gmail.com>
2015-01-16 11:50:37 -0600

    further playing with timezones

--- coderdojochi/models.py
+++ coderdojochi/models.py
@@ -231,7 +231,7 @@ class Session(models.Model):
         return orders
 
     def __unicode__(self):
-        return self.course.title + ' | ' + str(self.start_date)
+        return self.course.title + ' | ' + formats.date_format(self.start_date, "SHORT_DATETIME_FORMAT")
 
 class MeetingType(models.Model):
     code = models.CharField(max_length=255, blank=True, null=True)
