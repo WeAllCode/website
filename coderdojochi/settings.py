bf9411bee3b34e81be85d601d973af1139795460
Brett Kellgren <bkellgren@gmail.com>
2015-02-01 12:34:04 -0600

    cacheing updates
    Former-commit-id: d0c6a4b52aabb30015c28a3ef9b4ab3417fd01b8

--- coderdojochi/settings.py
+++ coderdojochi/settings.py
@@ -120,6 +120,12 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'
+    }
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
