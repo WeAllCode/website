f4004de978abd2297042b969f6f2a1395f4f08ff
Brett Kellgren <bkellgren@gmail.com>
2015-02-22 13:59:00 -0600

    include get_object_or_404 in signals
    Former-commit-id: b240a7d9f68cef8f720227b9ef719ef24b84b6fc

--- coderdojochi/signals_handlers.py
+++ coderdojochi/signals_handlers.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
 from django.dispatch import receiver
+from django.shortcuts import get_object_or_404
 
 from avatar.signals import avatar_updated
 
