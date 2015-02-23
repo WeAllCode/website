dbe4e82e751b0c095d3ca2e8e27757d68f900da6
Ali Karbassi <ali@karbassi.com>
2015-02-22 18:49:03 -0600

    Added django fields for the admin page.
    Former-commit-id: 711bccfae3ec0fc54b293294b03c0000c3052d5b

--- coderdojochi/admin.py
+++ coderdojochi/admin.py
@@ -5,18 +5,85 @@ from coderdojochi.models import Mentor, Guardian, Student, Course, Session, Orde
 User = get_user_model()
 
 class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_active', 'is_superuser', 'last_login', 'date_joined')
+    ordering = ['-date_joined']
+    list_per_page = 100
+    date_hierarchy = 'date_joined'
+    search_fields = ['first_name','last_name', 'email']
+
     change_form_template = 'loginas/change_form.html'
 
+class MentorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'user', 'active', 'has_attended_intro_meeting', 'public', 'created_at', 'updated_at')
+    ordering = ['-created_at']
+    list_per_page = 100
+    date_hierarchy = 'created_at'
+    search_fields = ['first_name','last_name', 'user']
+
+class GuardianAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'user', 'active', 'phone', 'zip', 'created_at', 'updated_at')
+    ordering = ['-created_at']
+    list_per_page = 100
+    date_hierarchy = 'created_at'
+    search_fields = ['first_name','last_name', 'user']
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'gender', 'guardian', 'created_at', 'updated_at', 'active')
+    ordering = ['guardian']
+    list_per_page = 100
+    date_hierarchy = 'created_at'
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('code', 'title', 'slug', 'created_at', 'updated_at')
+    ordering = ['created_at']
+    list_per_page = 100
+
+class SessionAdmin(admin.ModelAdmin):
+    list_display = ('course', 'start_date', 'end_date', 'location', 'capacity', 'active', 'public', 'announced_date')
+    ordering = ['start_date']
+    list_per_page = 100
+    date_hierarchy = 'start_date'
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('guardian', 'student', 'session', 'active', 'ip', 'check_in', 'alternate_guardian', 'affiliate', 'order_number', 'created_at', 'updated_at', 'week_reminder_sent', 'day_reminder_sent')
+    list_filter = ('check_in', 'session')
+    ordering = ['created_at']
+    list_per_page = 100
+    date_hierarchy = 'created_at'
+
+class MeetingTypeAdmin(admin.ModelAdmin):
+    pass
+
+class MeetingAdmin(admin.ModelAdmin):
+    pass
+
+class EquipmentTypeAdmin(admin.ModelAdmin):
+    pass
+
+class EquipmentAdmin(admin.ModelAdmin):
+    pass
+
+class DonationAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'amount', 'verified', 'receipt_sent', 'created_at', 'updated_at')
+    list_filter = ('amount', 'receipt_sent')
+    ordering = ['-created_at']
+    list_per_page = 100
+    date_hierarchy = 'created_at'
+
+class LocationAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(User, UserAdmin)
-admin.site.register(Mentor)
-admin.site.register(Guardian)
-admin.site.register(Student)
-admin.site.register(Course)
-admin.site.register(Session)
-admin.site.register(Order)
-admin.site.register(MeetingType)
-admin.site.register(Meeting)
-admin.site.register(EquipmentType)
-admin.site.register(Equipment)
-admin.site.register(Donation)
-admin.site.register(Location)
+admin.site.register(Mentor, MentorAdmin)
+admin.site.register(Guardian, GuardianAdmin)
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Session, SessionAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(MeetingType, MeetingTypeAdmin)
+admin.site.register(Meeting, MeetingAdmin)
+admin.site.register(EquipmentType, EquipmentTypeAdmin)
+admin.site.register(Equipment, EquipmentAdmin)
+admin.site.register(Donation, DonationAdmin)
+admin.site.register(Location, LocationAdmin)
