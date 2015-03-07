9ce7663d84d76e2c35c9baa9c1e9f3658ce01443
Ali Karbassi <ali@karbassi.com>
2015-03-07 14:22:32 -0600

    Adding deployment script.
    Former-commit-id: 4f5a7ba2652cf625265cdec53f8b5d410ee29566

---
+++ deploy.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+if [[ "$1" == "staging" ]]; then
+
+    echo "Deploying to staging..." &&
+    cd /home/coderdojo/webapps/coderdojochi_staging/ &&
+    source ../bin/activate &&
+    git checkout develop &&
+    git pull origin develop &&
+    pip install -r requirements.txt --exists-action=s &&
+    ((pip list | grep 'South') && pip uninstall -y South) &&
+    npm prune &&
+    npm install &&
+    ./node_modules/gulp/bin/gulp.js build &&
+    python2.7 manage.py makemigrations &&
+    python2.7 manage.py migrate &&
+    python2.7 manage.py collectstatic --noinput &&
+    /home/coderdojo/webapps/coderdojochi_staging/apache2/bin/restart
+
+elif [[ "$1" == "production" ]]; then
+
+    echo "Deploying to production..." &&
+    cd /home/coderdojo/webapps/coderdojochi/ &&
+    source ../bin/activate &&
+    git checkout master &&
+    git pull origin master &&
+    pip install -r requirements.txt --exists-action=s &&
+    ((pip list | grep 'South') && pip uninstall -y South) &&
+    npm prune &&
+    npm install &&
+    ./node_modules/gulp/bin/gulp.js build &&
+    python2.7 manage.py makemigrations &&
+    python2.7 manage.py migrate &&
+    python2.7 manage.py collectstatic --noinput &&
+    /home/coderdojo/webapps/coderdojochi_staging/apache2/bin/restart
+
+else
+
+    echo "Wrong deployment variable. deploy.sh [staging|production]"
+
+fi
