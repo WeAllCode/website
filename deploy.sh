bfb6209d508aa06a5531fb8b7efc0d065da489ce
Ali Karbassi <ali@karbassi.com>
2015-03-13 21:02:31 -0500

    Updated deployment script.
    Former-commit-id: 2eac1b415b79e2cca526a06fdf08a677d08bf06c

--- deploy.sh
+++ deploy.sh
@@ -2,32 +2,48 @@
 
 if [[ "$1" == "staging" ]]; then
 
-    echo "Deploying to staging..." &&
-    cd /home/coderdojo/webapps/coderdojochi_staging/coderdojochi &&
-    git checkout develop &&
+    echo "Deploying to staging..."
+    cd /home/coderdojo/webapps/coderdojochi_staging/coderdojochi
+    git checkout develop
     git pull origin develop
 
 elif [[ "$1" == "production" ]]; then
 
-    echo "Deploying to production..." &&
-    cd /home/coderdojo/webapps/coderdojochi/coderdojochi &&
-    git checkout master &&
+    echo "Deploying to production..."
+    cd /home/coderdojo/webapps/coderdojochi/coderdojochi
+    git checkout master
     git pull origin master
+
+elif [[ "$1" == "local" ]]; then
+
+    echo "Starting local..."
 
 else
 
-    echo "Wrong deployment variable. deploy.sh [staging|production]"
+    echo "Wrong deployment variable. deploy.sh [local|staging|production]"
     exit
 
 fi
 
-source ../bin/activate &&
-pip install -r requirements.txt --exists-action=s &&
-((pip list | grep 'South') && pip uninstall -y South) &&
-npm prune &&
-npm install &&
-./node_modules/gulp/bin/gulp.js build &&
-python2.7 manage.py makemigrations &&
-python2.7 manage.py migrate &&
-python2.7 manage.py collectstatic --noinput &&
-../apache2/bin/restart
+if [[ "$1" != "local" ]]; then
+    source ../bin/activate
+fi
+
+pip install -q -r requirements.txt --exists-action=s
+
+if [[ $(pip list | grep 'South') ]]; then
+    pip uninstall -q -y South
+fi
+
+npm prune
+npm install
+
+if [[ "$1" == "local" ]]; then
+    python manage.py syncdb
+else
+    ./node_modules/gulp/bin/gulp.js build
+    python2.7 manage.py makemigrations
+    python2.7 manage.py migrate
+    python2.7 manage.py collectstatic --noinput
+    ../apache2/bin/restart
+fi
