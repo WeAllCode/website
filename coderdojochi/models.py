4b53d99c597e8773bb6d587e2f9775f98cdde136
Brett Kellgren <bkellgren@gmail.com>
2015-05-12 00:05:58 -0500

    merge social auth
    Former-commit-id: b5cc7a35e759dd1c95e2d88b897ae887e4e8482a

--- coderdojochi/models.py
+++ coderdojochi/models.py
@@ -47,12 +47,10 @@ class CDCUser(AbstractUser):
         return user
 
     def create_user(self, username, email=None, password=None, **extra_fields):
-        return self._create_user(username, email, password, False, False,
-                                 **extra_fields)
+        return self._create_user(username, email, password, False, False, **extra_fields)
 
     def create_superuser(self, username, email, password, **extra_fields):
-        return self._create_user(username, email, password, True, True,
-                                 **extra_fields)
+        return self._create_user(username, email, password, True, True, **extra_fields)
 
     def get_absolute_url(self):
         return settings.SITE_URL + '/dojo'
