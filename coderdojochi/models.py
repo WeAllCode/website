15c2332ee28d56f338ab7f6e8319877d8d2bf161
Brett Kellgren <bkellgren@gmail.com>
2015-07-06 00:02:44 -0500

    allauth initial

--- coderdojochi/models.py
+++ coderdojochi/models.py
@@ -22,38 +22,6 @@ class CDCUser(AbstractUser):
     role = models.CharField(choices=Roles, max_length=10, blank=True, null=True)
     admin_notes = models.TextField(blank=True, null=True)
 
-    def _create_user(self, username, email, password, is_staff, is_superuser, **extra_fields):
-        """
-        Creates and saves a User with the given username, email and password.
-        """
-        now = timezone.now()
-        if not username:
-            raise ValueError('The given username must be set')
-        email = self.normalize_email(email)
-        user = self.model(
-            username=username,
-            email=email,
-            first_name=extra_fields[ 'first_name' ],
-            last_name=extra_fields[ 'last_name' ],
-            is_staff=is_staff,
-            is_active=True,
-            is_superuser=is_superuser,
-            last_login=now,
-            date_joined=now,
-            **extra_fields
-        )
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_user(self, username, email=None, password=None, **extra_fields):
-        return self._create_user(username, email, password, False, False,
-                                 **extra_fields)
-
-    def create_superuser(self, username, email, password, **extra_fields):
-        return self._create_user(username, email, password, True, True,
-                                 **extra_fields)
-
     def get_absolute_url(self):
         return settings.SITE_URL + '/dojo'
 
