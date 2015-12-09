b278b9e0832015a37662d99b7dec5fe2346c5380
Andrew Kos <akos@theonion.com>
2015-12-08 19:57:39 -0600

    nginx serving static files
    Former-commit-id: 7f296a602eda5002ff1a0b28c665340358187340

--- resources/js/build/gulp/tasks/copy.js
+++ resources/js/build/gulp/tasks/copy.js
@@ -4,8 +4,23 @@ var gulp = require('gulp');
 var config = require('../config.json');
 
 gulp.task('copy', function() {
-    return gulp.src([
-            config.img.src + '/**'
-        ])
-        .pipe(gulp.dest(config.img.dest));
+    gulp.src([
+        config.img.src + '/**'
+    ])
+      .pipe(gulp.dest(config.img.dest));
+
+    gulp.src([
+        config.css.src + '/**'
+    ])
+      .pipe(gulp.dest(config.css.dest));
+
+    gulp.src([
+        config.favico.src
+    ])
+      .pipe(gulp.dest(config.favico.dest));
+
+    gulp.src([
+        config.js.src + '/**'
+    ])
+      .pipe(gulp.dest(config.js.dest));
 });
