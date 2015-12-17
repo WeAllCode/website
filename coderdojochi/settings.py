dbafa4e59e7ceac51ec0308c136f04f2463d9915
Ali Karbassi <ali@karbassi.com>
2015-12-17 09:47:35 -0600

    Updated MEDIA and STATIC variables.

--- coderdojochi/settings.py
+++ coderdojochi/settings.py
@@ -146,32 +146,26 @@ USE_I18N = True
 USE_L10N = True
 
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: '/var/www/example.com/media/'
-MEDIA_ROOT = os.path.join(PACKAGE_ROOT, 'media')
-
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: 'http://example.com/media/', 'http://media.example.com/'
 MEDIA_URL = '/media/'
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' 'static/' subdirectories and in STATICFILES_DIRS.
-# Example: '/var/www/example.com/static/'
-MEDIA_ROOT = os.path.join(PACKAGE_ROOT, 'static')
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: '/var/www/example.com/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
+
 
 # URL prefix for static files.
 # Example: 'http://example.com/static/', 'http://static.example.com/'
 STATIC_URL = '/static/'
+STATIC_ROOT = '/build/static'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like '/home/html/static' or 'C:/www/django/static'.
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PACKAGE_ROOT, 'static'),
+    os.path.join(os.path.dirname(__file__), 'static'),
 )
+
 
 SESSION_SERIALIZER='django.contrib.sessions.serializers.PickleSerializer'
 
