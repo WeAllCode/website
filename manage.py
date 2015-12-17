2bc4bbc62917235b8b66014694a46643c5a4fc54
Ali Karbassi <ali@karbassi.com>
2015-12-16 23:17:39 -0600

    Remove debug code.

--- manage.py
+++ manage.py
@@ -3,8 +3,6 @@ import os
 import sys
 
 if __name__ == "__main__":
-    print os.environ
-
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coderdojochi.settings")
 
     from django.core.management import execute_from_command_line
