8278ff932d166e8db106cfa0783663813ab90f05
Ali Karbassi <ali@karbassi.com>
2016-01-09 22:56:28 -0600

    Upgraded npm packages. Replaced gulp-minify-css with gulp-cssnano as it’s no longer maintained.
    Former-commit-id: c444b0d055a99dcb65f81cca52d12891795b406b

--- resources/js/build/gulp/tasks/cssmin.js
+++ resources/js/build/gulp/tasks/cssmin.js
@@ -2,16 +2,13 @@
 
 var gulp = require('gulp');
 var config = require('../config.json');
-var minifyCSS = require('gulp-minify-css');
+var cssnano = require('gulp-cssnano');
 var rename = require('gulp-rename');
 var handleErrors = require('../utils/handle-errors');
 
 gulp.task('cssmin', function() {
     return gulp.src(config.css.dest + '/cdc.css')
         .pipe(rename('cdc.min.css'))
-        .pipe(minifyCSS({
-            keepSpecialComments: 0
-        }))
-        .on('error', handleErrors)
+        .pipe(cssnano())
         .pipe(gulp.dest(config.css.dest));
 });
