18bfad6b1d42047659cb39813a8fad4a46e8e138
Andrew Kos <akos123@gmail.com>
2016-02-02 20:17:26 -0600

    again

--- deploy-docker.sh
+++ deploy-docker.sh
@@ -5,11 +5,11 @@ EB_BUCKET=elasticbeanstalk-us-east-1-383928539406
 
 # Create new Elastic Beanstalk version
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
-sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
+sed "s/<TAG>/$SHA1/; s/<REPO>/$CIRCLE_PROJECT_REPONAME/;" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
-aws elasticbeanstalk create-application-version --application-name hello \
+aws elasticbeanstalk create-application-version --application-name coderdojochi \
   --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
 # Update Elastic Beanstalk environment to new version
-aws elasticbeanstalk update-environment --environment-name hello-env \
+aws elasticbeanstalk update-environment --environment-name coderdojochi-env \
     --version-label $SHA1
