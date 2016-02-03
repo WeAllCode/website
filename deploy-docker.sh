2b7639e03e5512cab4398edd446c9ad3760777fc
Andrew Kos <akos123@gmail.com>
2016-02-02 21:04:53 -0600

    broken build
    Former-commit-id: f1a0f7191f1c8a498bdb208e2829b24916e76128

--- deploy-docker.sh
+++ deploy-docker.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
 SHA1=$1
+REPONAME=$2
 EB_BUCKET=elasticbeanstalk-us-east-1-383928539406
+
+docker push kand/$REPONAME:$SHA1
 
 # Create new Elastic Beanstalk version
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
-sed "s/<TAG>/$SHA1/; s/<REPO>/$CIRCLE_PROJECT_REPONAME/;" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
+sed "s/<TAG>/$SHA1/; s/<REPO>/$REPONAME/;" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
+
+# TODO  : try these with aws commands
+eb init -p docker -r us-east-1 coderdojochi
+eb create -d coderdojochi-env
+
 aws elasticbeanstalk create-application-version --application-name coderdojochi \
   --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
