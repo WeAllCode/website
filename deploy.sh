1bd984135794cda87458cb91b32ee667913717e2
Ali Karbassi <ali@karbassi.com>
2016-03-01 17:58:04 -0600

    Updated env calls.
    Former-commit-id: c30267d6ed34c7f64fe3fe9642b80633e875bc44

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 python manage.py collectstatic --noinput
 
-while ! timeout 1 bash -c "echo > /dev/tcp/$DB_PORT_5432_TCP_ADDR/$DB_PORT_5432_TCP_PORT"; do sleep 10; done
+while ! timeout 1 bash -c "echo > /dev/tcp/$POSTGRES_HOST/$POSTGRES_PORT"; do sleep 2; done
 
 python manage.py migrate
 python manage.py loaddata \
