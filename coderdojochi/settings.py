601cd51b28df876f4fa23523188f46af0a7dd6c3
Ali Karbassi <ali@karbassi.com>
2016-03-08 15:02:19 -0600

    Updated nginx and installing memcached.

--- coderdojochi/settings.py
+++ coderdojochi/settings.py
@@ -132,7 +132,8 @@ CONTACT_EMAIL = 'info@coderdojochi.org'
 
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
     }
 }
 
