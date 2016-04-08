76fbf1a2c2ed44d280484cee91a7a55dcb82be68
Ali Karbassi <ali@karbassi.com>
2016-04-08 13:40:18 -0500

    trying uwsgi. who knows =\
    Former-commit-id: 0bbb0e0fa773ee66061e97e5919296f4fdaaffd5

--- deploy.sh
+++ deploy.sh
@@ -12,4 +12,6 @@ python manage.py loaddata \
     /fixtures/sites.json \
     /fixtures/socialaccount.json
 
-gunicorn --config $DIR_BUILD/gunicorn.conf.py coderdojochi.wsgi
+# gunicorn --config $DIR_BUILD/gunicorn.conf.py coderdojochi.wsgi
+uwsgi -s /tmp/uwsgi.sock -w coderdojochi:coderdojochi --chown-socket=www-data:www-data --enable-threads & \
+    nginx -g 'daemon off;'
