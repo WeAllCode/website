ad0b14b38cce5bbcb73fa5d077fcff57d43c12b3
Ali Karbassi <ali@karbassi.com>
2016-04-09 18:10:03 -0500

    Disable caching for testing.

--- coderdojochi/settings.py
+++ coderdojochi/settings.py
@@ -136,8 +136,9 @@ CONTACT_EMAIL = 'info@coderdojochi.org'
 
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': '127.0.0.1:11211',
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+        # 'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        # 'LOCATION': '127.0.0.1:11211',
     }
 }
 
