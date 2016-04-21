73de94d50ae7e4b523d515c545b14f0f4deed890
Ali Karbassi <ali@karbassi.com>
2016-04-20 19:03:05 -0500

    Fixed `loaddata`.

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,6 @@ python manage.py collectstatic --noinput
 while ! timeout 1 bash -c "echo > /dev/tcp/$POSTGRES_HOST/$POSTGRES_PORT"; do sleep 2; done
 
 python manage.py migrate
-python manage.py loaddata \
-    /fixtures/coderdojochi.json \
-    /fixtures/sites.json \
-    /fixtures/socialaccount.json
+python manage.py loaddata fixtures/*.json
 
 gunicorn --config $DIR_BUILD/gunicorn.conf.py coderdojochi.wsgi
