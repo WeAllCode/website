0f79d48b2f63065d840cd9b50cc5820e35d751d0
Ali Karbassi <ali@karbassi.com>
2016-05-13 14:01:28 -0500

    Enable AWS s3 storage for static files.
    Former-commit-id: 1e828b68fc0b9749bb9f8c4c783fd440eb5d5f1c

--- coderdojochi/settings.py
+++ coderdojochi/settings.py
@@ -200,9 +200,9 @@ if AWS_STORAGE_BUCKET_NAME and AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY:
     # We also use it in the next setting.
     AWS_S3_CUSTOM_DOMAIN = '{}.s3.amazonaws.com'.format(AWS_STORAGE_BUCKET_NAME)
 
-    # STATICFILES_LOCATION = 'static'
-    # STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-    # STATIC_URL = "https://{}/{}/".format(AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
+    STATICFILES_LOCATION = 'static'
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATIC_URL = "https://{}/{}/".format(AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
 
     MEDIAFILES_LOCATION = 'media'
     MEDIA_URL = "https://{}/{}/".format(AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
