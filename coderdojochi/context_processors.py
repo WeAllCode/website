2a0995da1f6c9534032dc23b1584a7715bd78967
Brett Kellgren <bkellgren@gmail.com>
2016-05-17 23:48:51 -0500

    cleanup

--- coderdojochi/context_processors.py
+++ coderdojochi/context_processors.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 
-def main_config_processor(request):
 
+def main_config_processor(request):
     return {
         'SITE_URL': settings.SITE_URL,
         'IS_PRODUCTION': settings.IS_PRODUCTION
