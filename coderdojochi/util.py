498edeb756d83720abc491b89c5a45779db22c35
Ali Karbassi <ali@karbassi.com>
2016-06-06 12:50:54 -0500

    For ICS files, change localtime to UTC.

--- coderdojochi/util.py
+++ coderdojochi/util.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import arrow
+
 def str_to_bool(s):
     if s == 'True':
         return True
@@ -8,3 +10,6 @@ def str_to_bool(s):
     else:
         # evil ValueError that doesn't tell you what the wrong value was
         raise ValueError
+
+def local_to_utc(date):
+    return arrow.get(date).replace(tzinfo='America/Chicago').to('UTC')
