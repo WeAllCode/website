7bd34cb7d65546ea1d2820b46fccf0a83d56582d
Ali Karbassi <ali@karbassi.com>
2017-01-09 19:36:41 -0600

    circleci

--- bin/push.sh
+++ bin/push.sh
@@ -6,12 +6,12 @@ IFS=$'\n\t'
 # 'latest' as the tag. 'latest' will always refer to the last image that was
 # built, since the next time this script is run, it'll get overridden. The
 # commit hash, however, is a constant reference to this image.
-docker tag -f $DOCKER_USER/$DOCKER_REPO $DOCKER_USER/$DOCKER_REPO:$CIRCLE_BUILD_NUM
+# docker tag -f $DOCKER_USER/$DOCKER_REPO $DOCKER_USER/$DOCKER_REPO:$CIRCLE_BUILD_NUM
 docker push $DOCKER_USER/$DOCKER_REPO:$CIRCLE_BUILD_NUM
-docker tag -f $DOCKER_USER/$DOCKER_REPO $DOCKER_USER/$DOCKER_REPO:latest
+# docker tag -f $DOCKER_USER/$DOCKER_REPO $DOCKER_USER/$DOCKER_REPO:latest
 docker push $DOCKER_USER/$DOCKER_REPO:latest
 
-docker tag -f $DOCKER_USER/$DOCKER_REPO-static $DOCKER_USER/$DOCKER_REPO-static:$CIRCLE_BUILD_NUM
+# docker tag -f $DOCKER_USER/$DOCKER_REPO-static $DOCKER_USER/$DOCKER_REPO-static:$CIRCLE_BUILD_NUM
 docker push $DOCKER_USER/$DOCKER_REPO-static:$CIRCLE_BUILD_NUM
-docker tag -f $DOCKER_USER/$DOCKER_REPO-static $DOCKER_USER/$DOCKER_REPO-static:latest
+# docker tag -f $DOCKER_USER/$DOCKER_REPO-static $DOCKER_USER/$DOCKER_REPO-static:latest
 docker push $DOCKER_USER/$DOCKER_REPO-static:latest
