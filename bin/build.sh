aec98681e11552f8189a0689ae8d4c36f26ebebc
Ali Karbassi <ali@karbassi.com>
2017-01-09 19:36:41 -0600

    circleci
    Former-commit-id: 7bd34cb7d65546ea1d2820b46fccf0a83d56582d

--- bin/build.sh
+++ bin/build.sh
@@ -1,2 +1,4 @@
 docker build -t "$DOCKER_USER/$DOCKER_REPO:$CIRCLE_BUILD_NUM" .
+docker build -t "$DOCKER_USER/$DOCKER_REPO:latest" .
 docker build -t "$DOCKER_USER/$DOCKER_REPO-static:$CIRCLE_BUILD_NUM" ./nginx
+docker build -t "$DOCKER_USER/$DOCKER_REPO-static:latest" ./nginx
