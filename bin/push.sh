ada408e3790aa4be57469c6a57b67fe396b9b17d
Ali Karbassi <ali@karbassi.com>
2017-01-09 22:40:45 -0600

    Move circleci tasks into bin/*.sh files

--- bin/push.sh
+++ bin/push.sh
@@ -2,16 +2,16 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-# Push same image twice, once with the commit hash as the tag, and once with
-# 'latest' as the tag. 'latest' will always refer to the last image that was
-# built, since the next time this script is run, it'll get overridden. The
-# commit hash, however, is a constant reference to this image.
-# docker tag -f $DOCKER_USER/$DOCKER_REPO $DOCKER_USER/$DOCKER_REPO:$CIRCLE_BUILD_NUM
+# Push same image twice.
+# - commit hash as the tag.
+# - 'latest' as the tag.
+#
+# 'latest' will always refer to the last image that was built, since the next
+# time this script is run, it'll get overridden. The commit hash, however, is a
+# constant reference to this image.
+
 docker push $DOCKER_USER/$DOCKER_REPO:$CIRCLE_BUILD_NUM
-# docker tag -f $DOCKER_USER/$DOCKER_REPO $DOCKER_USER/$DOCKER_REPO:latest
 docker push $DOCKER_USER/$DOCKER_REPO:latest
 
-# docker tag -f $DOCKER_USER/$DOCKER_REPO-static $DOCKER_USER/$DOCKER_REPO-static:$CIRCLE_BUILD_NUM
 docker push $DOCKER_USER/$DOCKER_REPO-static:$CIRCLE_BUILD_NUM
-# docker tag -f $DOCKER_USER/$DOCKER_REPO-static $DOCKER_USER/$DOCKER_REPO-static:latest
 docker push $DOCKER_USER/$DOCKER_REPO-static:latest
