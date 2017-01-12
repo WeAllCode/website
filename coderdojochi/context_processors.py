d7f143535cd440b05636b10c5cb7fbbb87a26002
Ali Karbassi <ali@karbassi.com>
2017-01-11 21:22:24 -0600

    Update static files (#444)
    Former-commit-id: 087db5de8766482a41d6ceb6b86a25f3b3afe016

--- coderdojochi/context_processors.py
+++ coderdojochi/context_processors.py
@@ -5,8 +5,8 @@ from django.conf import settings
 
 def main_config_processor(request):
     return {
-        'SITE_URL': settings.SITE_URL,
         'DEBUG': settings.DEBUG,
+        'SITE_URL': settings.SITE_URL,
         'STATIC_URL': settings.STATIC_URL,
         'CONTACT_EMAIL': settings.CONTACT_EMAIL,
     }
--- coderdojochi/urls.py
+++ coderdojochi/urls.py
@@ -272,8 +272,8 @@ urlpatterns = [
 ]
 
 urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
+    settings.STATIC_URL,
+    document_root=settings.STATIC_ROOT
 )
 
 if settings.DEBUG:
