916c006299095316c6a04ea42853b3fd65a4407f
Athina Verroiopoulou <athina.verroiopoulou@gmail.com>
2017-09-05 12:08:30 -0500

    Convert "about page" to class-based views (#508)
    * Convert "about page" to class-based views
     -Create about page to new views
     -Change the url
     -Delete about page from old views
    
    * Fix the requested changes to issue 508
     -Change the about function in the new views file
     -Made the requested changes to the about.html template
     - #508
    
    * Change the about view page
    - Change the about.html to estimate the students served and count the mentors
    - Change the about view in the general.py file

--- src/coderdojochi/views/general.py
+++ src/coderdojochi/views/general.py
@@ -87,7 +87,26 @@ class HomeView(TemplateView):
             upcoming_classes = upcoming_classes.filter(is_public=True)
 
         return upcoming_classes[:3]
+      
+class AboutView(TemplateView):
+    template_name = "about.html"
 
+    def get_context_data(self, **kwargs):
+        context = super(AboutView, self).get_context_data(**kwargs)
+
+        # Number of active mentors
+        context['mentor_count'] = Mentor.objects.filter(
+            is_active=True,
+            is_public=True,
+        ).count()
+
+        # Number of served students based on checkin counts
+        context['students_served_count'] = Order.objects.exclude(
+            is_active=False,
+            check_in=None
+        ).count()
+
+        return context
 
 class WelcomeView(TemplateView):
     template_name = "welcome.html"
