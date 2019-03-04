dd55b22761a629381397ed18759965ddc97f842b
Dan Conrad <dan.m.conrad@gmail.com>
2019-03-03 20:06:58 -0600

    Updates links in header/footer

--- weallcode/urls.py
+++ weallcode/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 
 urlpatterns = [
-    path('',                views.HomeView.as_view()),
-    path('our-story',       views.OurStoryView.as_view()),
-    path('learn',           views.LearnView.as_view()),
-    path('team',            views.TeamView.as_view()),
-    path('get-involved',    views.GetInvolvedView.as_view()),
+    path('',                views.HomeView.as_view(),           name='weallcode_home'),
+    path('our-story',       views.OurStoryView.as_view(),       name='weallcode_our_story'),
+    path('learn',           views.LearnView.as_view(),          name='weallcode_learn'),
+    path('team',            views.TeamView.as_view(),           name='weallcode_team'),
+    path('get-involved',    views.GetInvolvedView.as_view(),    name='weallcode_get_involved'),
 ]
