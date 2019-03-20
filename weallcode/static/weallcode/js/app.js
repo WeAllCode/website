154b9152293afe667d010f024ee444dffa63b9b7
Dan Conrad <dconrad@wyzant.com>
2019-03-19 19:40:42 -0500

    Adds more content for our story

--- weallcode/static/weallcode/js/app.js
+++ weallcode/static/weallcode/js/app.js
@@ -1,1 +1,14 @@
 $(document).foundation()
+
+$(document).ready(function() {
+  $('a[href*="#"]').click(function(e) {
+    e.preventDefault();
+
+    const val = $(this).attr('href');
+    const id = val.substr(val.indexOf('#'));
+
+    $([document.documentElement, document.body]).animate({
+      scrollTop: $(id).offset().top
+    }, 1500);
+  });
+});
