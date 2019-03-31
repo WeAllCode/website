79a627c01488b0407e9c5140fa4e1fa1321433d7
Dan Conrad <dan.m.conrad@gmail.com>
2019-03-31 17:38:16 -0500

    Adds rendering of contact form

---
+++ weallcode/forms.py
@@ -0,0 +1,62 @@
+
+from django import forms
+from django.forms import Form
+
+
+class GridForm(Form):
+    def to_grid(self):
+        return ''.join([self.field_html(f[0], f[1]) for f in self.widths])
+
+    def field_html(self, field_name, field_classes):
+        field = self[field_name]
+        return f"<div class='cell {field_classes}'>{field.label_tag()}{field}{field.errors}</div>"
+
+
+class ContactForm(GridForm):
+    widths = (
+        ('name', 'small-6'),
+        ('email', 'small-6'),
+        ('interest', 'small-6'),
+        ('phone', 'small-6'),
+        ('message', 'small-12'),
+        ('human', 'hide'),
+    )
+
+    human = forms.CharField(
+        max_length=100,
+        label=False,
+        required=False
+    )
+
+    name = forms.CharField(
+        max_length=100,
+        label='Full Name',
+    )
+
+    email = forms.EmailField(
+        max_length=200,
+        label='Email Address',
+        widget=forms.TextInput(attrs={'placeholder': 'email@example.com'})
+    )
+
+    interest = forms.ChoiceField(
+        choices=[
+            ('volunteer', 'Volunteer'),
+            ('donate', 'Donate'),
+            ('sponsor', 'Sponsor'),
+            ('collaborate', 'Collaborate'),
+            ('other', 'Other'),
+        ],
+        label='Topic of Interest'
+    )
+
+    phone = forms.CharField(
+        max_length=20,
+        label='Phone Number',
+        widget=forms.TextInput(attrs={'type': 'tel', 'placeholder': '+1 555 555-5555'})
+    )
+
+    message = forms.CharField(
+        label='Message',
+        widget=forms.Textarea(attrs={'placeholder': 'Enter your message'})
+    )
--- weallcode/views.py
+++ weallcode/views.py
@@ -1,7 +1,11 @@
-from coderdojochi.models import Mentor, Session
+from django.contrib import messages
 from django.db.models import Count
 from django.utils import timezone
 from django.views.generic import TemplateView
+
+from weallcode.forms import ContactForm
+
+from coderdojochi.models import Mentor, Session
 
 
 class HomeView(TemplateView):
@@ -61,10 +65,42 @@ class TeamView(TemplateView):
         context['mentors'] = mentors
         context['top_volunteers'] = volunteers[0:8]
         context['other_volunteers'] = volunteers[8:]
+        return context
 
 
 class GetInvolvedView(TemplateView):
     template_name = "weallcode/get_involved.html"
+
+    def get_context_data(self):
+        return { "form": ContactForm().to_grid() }
+
+    def post(self, request, **kwargs):
+        if request.POST['human']:
+            return messages.error(request, "Bad robot.")
+
+        form = ContactForm(request.POST)
+
+        if form.is_valid():
+            # email(
+            #     subject=f"{request.POST['name']} | We All Code Contact Form",
+            #     recipients=[settings.CONTACT_EMAIL],
+            #     reply_to=[f"{request.POST['name']}<{request.POST['email']}>"],
+            #     template_name='contact-email',
+            #     merge_global_data={
+            #         'message': request.POST['message'],
+            #     },
+            # )
+
+            messages.success(
+                request,
+                "Thank you for contacting us! We will respond as soon as possible."
+            )
+
+            form = ContactForm()
+        else:
+            messages.error(request, "There was an error. Please try again.")
+
+        self.get_context_data(**kwargs)['form'] = form.to_grid()
 
 
 class PrivacyView(TemplateView):
