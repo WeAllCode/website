5b2e057a945cdc0ae4a40d3f1b7bc1532952a213
Dan Conrad <dconrad@wyzant.com>
2019-04-01 11:03:51 -0500

    Adds custom save logic according to github issue

--- coderdojochi/settings.py
+++ coderdojochi/settings.py
@@ -249,8 +249,29 @@ else:
     from storages.backends.s3boto3 import S3Boto3Storage  # noqa E402
     from django.contrib.staticfiles.storage import ManifestFilesMixin
 
+    # Taken from an issue in django-storages:
+    # https://github.com/jschneier/django-storages/issues/382#issuecomment-377174808
     class CustomS3Storage(ManifestFilesMixin, S3Boto3Storage):
-        pass
+        def _save_content(self, obj, content, parameters):
+            """
+            We create a clone of the content file as when this is passed to boto3 it wrongly closes
+            the file upon upload where as the storage backend expects it to still be open
+            """
+            # Seek our content back to the start
+            content.seek(0, os.SEEK_SET)
+
+            # Create a temporary file that will write to disk after a specified size
+            content_autoclose = SpooledTemporaryFile()
+
+            # Write our original content into our copy that will be closed by boto3
+            content_autoclose.write(content.read())
+
+            # Upload the object which will auto close the content_autoclose instance
+            super(CustomS3Boto3Storage, self)._save_content(obj, content_autoclose, parameters)
+
+            # Cleanup if this is fixed upstream our duplicate should always close
+            if not content_autoclose.closed:
+                content_autoclose.close()
 
     StaticRootS3BotoStorage = lambda: CustomS3Storage(location='static')
     MediaRootS3BotoStorage  = lambda: S3Boto3Storage(location='media', file_overwrite=False)
