1bbfe0bd5b9c24736a51021ab6548e5594c3f9a0
Daniel Conrad <3791768+danielmconrad@users.noreply.github.com>
2020-04-07 15:28:48 -0500

    Adds Slack Notifications (#769)
    * Adds Slack Notifications
    
    * Removes print
    
    * Removes unused variable
    
    * Adds SLACK_WEBHOOK_URL to required config
    
    * Fixes slack config
    
    * Removes unused space

---
+++ coderdojochi/notifications.py
@@ -0,0 +1,134 @@
+import logging
+
+import requests
+from django.conf import settings
+
+logger = logging.getLogger(__name__)
+
+
+class SlackNotification:
+    DEFAULT_PAYLOAD = {
+        "channel": settings.SLACK_ALERTS_CHANNEL,
+        "text": "IMPLEMENT_ME"
+    }
+
+    def __init__(self):
+        self.payload = DEFAULT_PAYLOAD
+
+    def send(self):
+        res = requests.post(settings.SLACK_WEBHOOK_URL, json={
+            **self.DEFAULT_PAYLOAD,
+            **self.payload
+        })
+
+        if res.status_code != requests.codes.ok:
+            logger.error({
+                "msg": "Unable to send Slack notification",
+                "error": res.content
+            })
+
+
+class NewMentorNotification(SlackNotification):
+    def __init__(self, mentor):
+        self.payload = {
+            "blocks": [
+                {
+                    "type": "divider"
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "text": "👋 New mentor signup!",
+                        "type": "mrkdwn"
+                    }
+                },
+                {
+                    "type": "section",
+                    "fields": [
+                        {
+                            "type": "mrkdwn",
+                            "text": f"*Name*: \n{mentor.user.name}"
+                        },
+                        {
+                            "type": "mrkdwn",
+                            "text": f"*Email*: \n{mentor.user.email}"
+                        }
+                    ]
+                }
+            ]
+        }
+
+
+class NewMentorOrderNotification(SlackNotification):
+    def __init__(self, mentor_order):
+        name = mentor_order.mentor.user.name
+        email = mentor_order.mentor.user.email
+        location = mentor_order.session.location.name
+        start_date = mentor_order.session.start_date.strftime("%Y-%m-%d")
+
+        self.payload = {
+            "blocks": [
+                {
+                    "type": "divider"
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "🏫 New mentor enrollment!"
+                    }
+                },
+                {
+                    "type": "section",
+                    "fields": [
+                        {
+                            "type": "mrkdwn",
+                            "text": f"*Name*: \n{name}\n"
+                        },
+                        {
+                            "type": "mrkdwn",
+                            "text": f"*Email*: \n{email}\n"
+                        },
+                        {
+                            "type": "mrkdwn",
+                            "text": f"*Location*: \n{location}"
+                        },
+                        {
+                            "type": "mrkdwn",
+                            "text": f"*Date*: \n{start_date}"
+                        }
+                    ]
+                }
+            ]
+        }
+
+
+class NewMentorBgCheckNotification(SlackNotification):
+    def __init__(self, mentor):
+        self.payload = {
+            "blocks": [
+                {
+                    "type": "divider"
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "✅ New mentor background check!"
+                    }
+                },
+                {
+                    "type": "section",
+                    "fields": [
+                        {
+                            "type": "mrkdwn",
+                            "text": f"*Name*: \n{mentor.user.name}"
+                        },
+                        {
+                            "type": "mrkdwn",
+                            "text": f"*Email*: \n{mentor.user.email}"
+                        }
+                    ]
+                }
+            ]
+        }
