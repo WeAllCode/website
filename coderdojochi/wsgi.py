c1f726cbbcbe35256f9aa4cf21949f05570162d6
Ali Karbassi <ali@karbassi.com>
2020-05-08 19:30:33 -0500

    Upgrade Sentry Fixes #773

--- coderdojochi/wsgi.py
+++ coderdojochi/wsgi.py
@@ -11,8 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coderdojochi.settings")
 
-application = Sentry(get_wsgi_application())
+application = get_wsgi_application()
