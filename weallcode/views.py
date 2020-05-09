b213fb33742d955252b79636066923a0860157f4
Ali Karbassi <ali@karbassi.com>
2020-05-08 21:03:20 -0500

    Add Sentry 404 catcher Fixes #775

--- weallcode/views.py
+++ weallcode/views.py
@@ -5,11 +5,13 @@ from django.conf import settings
 from django.contrib import messages, sitemaps
 from django.contrib.auth import get_user_model
 from django.db.models import Count
+from django.http import HttpResponseNotFound
 from django.urls import reverse, reverse_lazy
 from django.utils import timezone
 from django.views.generic import FormView, TemplateView
 
 from meta.views import MetadataMixin
+from sentry_sdk import capture_message
 
 from coderdojochi.models import Course, Mentor, Session
 
@@ -17,6 +19,16 @@ from .forms import ContactForm
 from .models import AssociateBoardMember, BoardMember, StaffMember
 
 User = get_user_model()
+
+
+def page_not_found_view(*args, **kwargs):
+    print('page_not_found_view')
+    capture_message("Page not found!", level="error")
+
+    return render(request, "404.html", {
+            'sentry_event_id': last_event_id(),
+            'SENTRY_DSN': settings.SENTRY_DSN,
+        }, status=404)
 
 
 class DefaultMetaTags(MetadataMixin):
