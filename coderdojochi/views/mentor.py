36b0932a40c57a8367576d87cddedc52ee147cf4
Ali Karbassi <ali@karbassi.com>
2021-01-08 15:03:59 -0600

    Remove inactive mentors from mentor master list (#835)
    - Update mentor list and mentor detail views to class-based-views
    - Remove inactive mentors from showing up on list and detail views

---
+++ coderdojochi/views/mentor.py
@@ -0,0 +1,33 @@
+from django.views.generic import DetailView, ListView
+
+from ..models import Mentor
+
+
+class MentorListView(ListView):
+    model = Mentor
+
+    def get_queryset(self):
+        return (
+            Mentor.objects.filter(
+                user__is_active=True,
+                is_active=True,
+                is_public=True,
+                background_check=True,
+                avatar_approved=True,
+            )
+            .select_related("user")
+            .order_by("user__date_joined")
+        )
+
+
+class MentorDetailView(DetailView):
+    model = Mentor
+
+    def get_queryset(self):
+        return Mentor.objects.filter(
+            user__is_active=True,
+            is_active=True,
+            is_public=True,
+            background_check=True,
+            avatar_approved=True,
+        ).select_related("user")
---
+++ fixtures-prod/get-all.sh
@@ -0,0 +1,29 @@
+#/bin/env bash
+
+# To run:
+# command time ./get-all.sh
+
+models=(
+    "sites.site"
+    "auth.group"
+    "socialaccount.socialapp"
+    "redirects.redirect"
+    "coderdojochi.cdcuser"
+    "coderdojochi.course"
+    "coderdojochi.equipmenttype"
+    "coderdojochi.location"
+    "coderdojochi.meetingtype"
+    "coderdojochi.raceethnicity"
+    "coderdojochi.guardian"
+    "coderdojochi.meeting"
+    "coderdojochi.mentor"
+    "coderdojochi.student"
+    "coderdojochi.session"
+    "coderdojochi.mentororder"
+    "coderdojochi.order"
+    "weallcode.boardmember"
+    "weallcode.associateboardmember"
+    "weallcode.staffmember"
+)
+
+heroku run --app production-wac python -W ignore manage.py dumpdata "${models[@]}" -- > "all.json"
