da85b3160076f78d7b9a8174eaa70f1bc2ed8c9c
Robert Groves <rwgdev@gmail.com>
2021-01-13 22:43:41 -0600

    Add missing context values required by template.

--- coderdojochi/views/mentor/sessions.py
+++ coderdojochi/views/mentor/sessions.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404
 from django.views.generic import DetailView
 
-from ...models import Mentor, Session
+from ...models import Mentor, MentorOrder, Session
 
 
 class SessionDetailView(DetailView):
@@ -9,8 +9,18 @@ class SessionDetailView(DetailView):
     template_name = "mentor/session-detail.html"
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-
+        session = self.object
         mentor = get_object_or_404(Mentor, user=self.request.user)
+
+        session_orders = MentorOrder.objects.filter(
+            session=session,
+            mentor=mentor,
+            is_active=True,
+        )
+
+        context = super().get_context_data(**kwargs)
+        context["mentor_signed_up"] = session_orders.exists()
+        context["spots_remaining"] = session.get_mentor_capacity() - session_orders.count()
+        context["account"] = mentor
 
         return context
