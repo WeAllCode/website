adc33755ded1ab50cbab08437bfc6eeb55881c19
Robert Groves <rwgdev@gmail.com>
2021-02-02 14:54:44 -0600

    Update student_register_link tag.
    The tag will now disable the enroll button when a student does not meet the prerequisites for a session.
    
    It will also add buttons for prerequisite course sessions.
    
    These buttons will navigate to the registration page of an upcoming session for the prerequisite, if avaialable.

--- coderdojochi/templatetags/coderdojochi_extras.py
+++ coderdojochi/templatetags/coderdojochi_extras.py
@@ -38,6 +38,7 @@ def student_register_link(context, student, session):
     button_additional_attributes = ""
     button_msg = "Enroll"
     button_href = f"href={url}"
+    prerequisite_needed_buttons = ""
 
     if orders.count():
         button_modifier = "tertiary"
@@ -73,7 +74,44 @@ def student_register_link(context, student, session):
             message = f"Sorry, this class is limited to {session.gender_limitation}s this time around."
             button_href = f'data-trigger="hover" data-placement="top" data-toggle="popover" title="" data-content="{message}" data-original-title="{title}" '
 
+    else:
+        # Get the prerequisites the student still needs to take in order to attend the current session.
+        course_prerequisites_needed = list(session.get_course_prerequisites_needed(student).values("id", "code"))
+
+        if len(course_prerequisites_needed) > 0:
+            # When there are outstanding prerequisites, add buttons to navigate to future sessions (if available; disabled otherwise).
+            for course_prerequisite in course_prerequisites_needed:
+                needed_course_id = course_prerequisite["id"]
+                upcoming_prerequisite_sessions = context["upcoming_prerequisite_sessions"]
+
+                if needed_course_id in upcoming_prerequisite_sessions:
+                    session_id = upcoming_prerequisite_sessions[needed_course_id]["id"]
+                    url = reverse("session-detail", kwargs={"pk": session_id})
+                    button_tag = "a"
+                    button_href = f"href='{url}'"
+                    title = "There is an upcoming session for this prerequisite."
+                    button_additional_attributes = f"data-tooltip aria-haspopup='true' title='{title}'"
+                else:
+                    button_tag = "span"
+                    button_href = ""
+                    title = "No upcoming session for this prerequisite."
+                    button_additional_attributes = f"data-tooltip aria-haspopup='true' title='{title}' disabled"
+
+                button_modifier = "has-tip"
+                button_msg = course_prerequisite["code"]
+                prerequisite_needed_buttons += f"<{button_tag} {button_href} class='button {button_modifier}' {button_additional_attributes}>{button_msg}</{button_tag}>"
+
+            button_tag = "span"
+            button_modifier = "has-tip"
+            button_additional_attributes = "disabled"
+            button_msg = "Enroll"
+            title = f"Sorry, this class has prerequisites that have not been met by {student.first_name} yet."
+            button_href = f'data-tooltip aria-haspopup="true" title="{title}"'
+
     form = f"<{button_tag} {button_href} class='button small {button_modifier}' {button_additional_attributes}>{button_msg}</{button_tag}>"
+
+    if prerequisite_needed_buttons:
+        form += f"<br /><div class='tiny button-group align-right'>{prerequisite_needed_buttons}</div>"
 
     return Template(form).render(context)
 
