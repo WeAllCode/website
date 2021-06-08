4fbc91f70ab5e2cb8ef17e57ecd369d2c137e983
Linda Ramos <ltramos7@gmail.com>
2021-06-08 15:03:46 -0600

    added SessionCancelView to admin, mentors

--- coderdojochi/views/mentor/sessions.py
+++ coderdojochi/views/mentor/sessions.py
@@ -1,4 +1,5 @@
-from django.shortcuts import get_object_or_404
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import get_object_or_404, redirect
 from django.views.generic import DetailView
 
 from ...models import Mentor, MentorOrder, Session
@@ -14,12 +15,11 @@ class SessionDetailView(DetailView):
 
         session_orders = MentorOrder.objects.filter(
             session=session,
-            mentor=mentor,
             is_active=True,
         )
 
         context = super().get_context_data(**kwargs)
-        context["mentor_signed_up"] = session_orders.exists()
+        context["mentor_signed_up"] = session_orders.filter(mentor=mentor).exists()
         context["spots_remaining"] = session.mentor_capacity - session_orders.count()
         context["account"] = mentor
 
@@ -29,5 +29,51 @@ class SessionDetailView(DetailView):
                 is_active=True,
             ).values("mentor__id")
         )
+
+        return context
+
+
+class SessionCancelView(DetailView):
+    model = Session
+    template_name = "mentor/session_cancel.html"
+
+    # redirect if order not found
+    def get(self, request, *args, **kwargs):
+        try: 
+            self.object = self.get_object()
+            session = self.object
+            mentor = get_object_or_404(Mentor, user=self.request.user)
+            mentor_order = MentorOrder.objects.get(
+                session=session,
+                mentor=mentor,
+                is_active=True,
+            )
+            context = self.get_context_data(object=self.object)
+            return self.render_to_response(context) 
+        except ObjectDoesNotExist:
+            return redirect(session.get_absolute_url())
+
+
+    def get_context_data(self, **kwargs):
+        session = self.object
+        mentor = get_object_or_404(Mentor, user=self.request.user)
+
+        mentor_order = MentorOrder.objects.get(
+            session=session,
+            mentor=mentor,
+            is_active=True,
+        )
+
+        context = super().get_context_data(**kwargs)
+        # context["mentor_signed_up"] = session_orders.filter(mentor=mentor).exists()
+        # context["spots_remaining"] = session.mentor_capacity - session_orders.count()
+        # context["account"] = mentor
+
+        # context["active_mentors"] = Mentor.objects.filter(
+        #     id__in=MentorOrder.objects.filter(
+        #         session=self.object,
+        #         is_active=True,
+        #     ).values("mentor__id")
+        # )
 
         return context
