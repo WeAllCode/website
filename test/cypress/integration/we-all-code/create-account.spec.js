a710d911466c38f32c7bc1e43c8f6ab9eb35ccf6
Ali Karbassi <ali@karbassi.com>
2022-03-16 15:29:53 -0500

    Fix tests

--- test/cypress/integration/we-all-code/create-account.spec.js
+++ test/cypress/integration/we-all-code/create-account.spec.js
@@ -1,12 +1,12 @@
 /// <reference types="cypress" />
 
-const faker = require("faker");
+const { faker } = require("@faker-js/faker");
 
 describe("Signup User", () => {
   beforeEach(() => {
     const first_name = faker.name.firstName();
     const last_name = faker.name.lastName();
-    const email = faker.internet.exampleEmail(first_name,last_name);
+    const email = faker.internet.exampleEmail(first_name, last_name);
     const password = faker.internet.password();
 
     cy.visit("/account/signup/");
@@ -23,19 +23,18 @@ describe("Signup User", () => {
   });
 
   it("Parent Signup", function () {
-
     const student_first_name = faker.name.firstName();
     const student_last_name = faker.name.lastName();
     const phone = faker.phone.phoneNumber();
-    const birthday = isParent => {
-      const max = isParent ? 2000 : 2005
-      const min = isParent ? 1950 : 2013
-      const year = Math.floor(Math.random() * (max - min + 1)) + min
-      const month = Math.floor((Math.random() * 12)) + 1
-      const formattedMonth = month < 10 ? `0${month}` : `${month}`
-      const day = Math.floor((Math.random() * 27)) + 1
-      const formattedDay = day < 10 ? `0${day}` : `${day}`
-      return `${year}-${formattedMonth}-${formattedDay}`
+    const birthday = (isParent) => {
+      const max = isParent ? 2000 : 2005;
+      const min = isParent ? 1950 : 2013;
+      const year = Math.floor(Math.random() * (max - min + 1)) + min;
+      const month = Math.floor(Math.random() * 12) + 1;
+      const formattedMonth = month < 10 ? `0${month}` : `${month}`;
+      const day = Math.floor(Math.random() * 27) + 1;
+      const formattedDay = day < 10 ? `0${day}` : `${day}`;
+      return `${year}-${formattedMonth}-${formattedDay}`;
     };
     const gender = faker.name.gender();
     const zip = faker.address.zipCode();
@@ -43,13 +42,13 @@ describe("Signup User", () => {
 
     cy.get(".medium-offset-2 > p > .button").click();
 
-    cy.get("#id_phone").type(phone);
+    cy.get("#idfaker.phone").type(phone);
 
     cy.get("#id_zip").type(zip);
 
     cy.get("#id_gender").type("Male");
     cy.get("#id_race_ethnicity").select([`${ethnicity}`]);
-    cy.get("#id_birthday").type(birthday(true))
+    cy.get("#id_birthday").type(birthday(true));
     cy.contains("Continue").click();
 
     cy.get("#id_first_name").type(student_first_name);
@@ -65,25 +64,25 @@ describe("Signup User", () => {
     cy.get("#id_school_name").type("Example School");
     cy.get("#id_school_type > :nth-child(1) > label").click();
     cy.get("#id_school_type_0").check();
-    cy.get("#id_medical_conditions").type('None');
-    cy.get("#id_medications").type('None')
+    cy.get("#id_medical_conditions").type("None");
+    cy.get("#id_medications").type("None");
     cy.get("#id_photo_release").check();
     cy.get(":nth-child(11) > .checkbox > label").click();
     cy.get("#id_consent").check();
     cy.get(".submit").click();
-    /* ==== End Cypress Studio ==== */
+    
   });
 
   it("Volunteer Signup", () => {
     const volunteer_birthday = () => {
-      const max = 2000
-      const min = 1980
-      const year = Math.floor(Math.random() * (max - min + 1)) + min
-      const month = Math.floor((Math.random() * 12)) + 1
-      const formattedMonth = month < 10 ? `0${month}` : `${month}`
-      const day = Math.floor((Math.random() * 27)) + 1
-      const formattedDay = day < 10 ? `0${day}` : `${day}`
-      return `${year}-${formattedMonth}-${formattedDay}`
+      const max = 2000;
+      const min = 1980;
+      const year = Math.floor(Math.random() * (max - min + 1)) + min;
+      const month = Math.floor(Math.random() * 12) + 1;
+      const formattedMonth = month < 10 ? `0${month}` : `${month}`;
+      const day = Math.floor(Math.random() * 27) + 1;
+      const formattedDay = day < 10 ? `0${day}` : `${day}`;
+      return `${year}-${formattedMonth}-${formattedDay}`;
     };
     const gender = faker.name.gender();
     const workplace = faker.company.companyName();
@@ -91,23 +90,23 @@ describe("Signup User", () => {
     const address = faker.address.streetAddress(true);
     const ethnicity = Math.floor(Math.random() * 5) + 1;
 
-    cy.contains("Volunteer").click()
+    cy.contains("Volunteer").click();
 
     /* ==== Generated with Cypress Studio ==== */
-    cy.get('#id_bio').type("Lorem ipsum");
+    cy.get("#id_bio").type("Lorem ipsum");
 
-    cy.get('#id_birthday').type(volunteer_birthday());
+    cy.get("#id_birthday").type(volunteer_birthday());
 
-    cy.get('#id_gender').type(gender);
- 
-    cy.get('#id_work_place').type(workplace);
+    cy.get("#id_gender").type(gender);
 
-    cy.get('#id_phone').type(phone);
+    cy.get("#id_work_place").type(workplace);
 
-    cy.get('#id_home_address').type(address);
+    cy.get("#idfaker.phone").type(phone);
 
-    cy.get('#id_race_ethnicity').select([`${ethnicity}`]);
-    cy.get('.submit').click();
-    /* ==== End Cypress Studio ==== */
-  })
+    cy.get("#id_homefaker.address").type(address);
+
+    cy.get("#id_race_ethnicity").select([`${ethnicity}`]);
+    cy.get(".submit").click();
+
+  });
 });
--- test/cypress/plugins/index.js
+++ test/cypress/plugins/index.js
@@ -12,11 +12,22 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+const del = require("del");
+
 /**
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
+
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-}
+
+  on("after:spec", (spec, results) => {
+    if (results && results.stats.failures === 0 && results.video) {
+      // `del()` returns a promise, so it's important to return it to ensure
+      // deleting the video is finished before moving on
+      return del(results.video);
+    }
+  });
+};
