c12c899f646bcd0fc922d3d7ca77bfa9fdff81e3
Ali Karbassi <ali@karbassi.com>
2023-04-13 14:22:09 -0500

    Upgrade sentry

--- coderdojochi/settings.py
+++ coderdojochi/settings.py
@@ -17,8 +17,6 @@ from django.conf.locale.en import formats as en_formats
 import dj_database_url
 import django_heroku
 import environ
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
 
 env = environ.Env()
 
@@ -387,18 +385,22 @@ SLACK_WEBHOOK_URL = env("SLACK_WEBHOOK_URL")
 SLACK_ALERTS_CHANNEL = env("SLACK_ALERTS_CHANNEL", default=None)
 
 # Sentry
-SENTRY_DSN = env("SENTRY_DSN")
-sentry_sdk.init(
-    dsn=SENTRY_DSN,
-    integrations=[DjangoIntegration()],
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
-    traces_sample_rate=1.0,
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True,
-)
+SENTRY_DSN = env("SENTRY_DSN", default=False)
+if SENTRY_DSN:
+    import sentry_sdk
+    from sentry_sdk.integrations.django import DjangoIntegration
+
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,
+        integrations=[DjangoIntegration()],
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0,
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True,
+    )
 
 
 # Django 3.2.x fix
--- weallcode/views/common.py
+++ weallcode/views/common.py
@@ -1,54 +1,26 @@
-from collections import defaultdict
-from datetime import datetime
-from itertools import chain
-
 from django.conf import settings
-from django.contrib import (
-    messages,
-    sitemaps,
-)
 from django.contrib.auth import get_user_model
-from django.db.models import Count
-from django.urls import (
-    reverse,
-    reverse_lazy,
-)
-from django.utils import timezone
-from django.views.generic import (
-    FormView,
-    TemplateView,
-)
 
 from meta.views import MetadataMixin
 from sentry_sdk import capture_message
-
-from coderdojochi.models import (
-    Course,
-    Mentor,
-    Session,
-)
-
-from ..forms import ContactForm
-from ..models import (
-    AssociateBoardMember,
-    BoardMember,
-    StaffMember,
-)
 
 User = get_user_model()
 
 
 def page_not_found_view(*args, **kwargs):
     print("page_not_found_view")
-    capture_message("Page not found!", level="error")
+
+    options = {}
+
+    if settings.SENTRY_DSN:
+        capture_message("Page not found!", level="error")
+        options["sentry_event_id"] = last_event_id()
+        options["SENTRY_DSN"] = settings.SENTRY_DSN
 
     return render(
         request,
         "404.html",
-        {
-            "sentry_event_id": last_event_id(),
-            "SENTRY_DSN": settings.SENTRY_DSN,
-        },
+        options,
         status=404,
     )
 
